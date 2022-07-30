e6aad646c9c1d6604122865b13ea28cd87df6ac9
david-codez <davidcodesyt@gmail.com>
2022-07-30 01:36:53 -0400

    fixing image bug

--- components/HangmanFigure.jsx
+++ components/HangmanFigure.jsx
@@ -1,24 +1,35 @@
 import React, {useState} from 'react'
 import Image from 'next/image'
-// import hangman1 from '/images/hangman-1.png'
-// import hangman2 from '/images/hangman-2'
-// import hangman3 from '/images/hangman-3'
-// import hangman4 from '/images/hangman-4'
-// import hangman5 from '/images/hangman-5'
-// import hangman6 from '/images/hangman-6'
+import hangman0 from '../public/images/hangman-0.png'
+import hangman1 from '../public/images/hangman-1.png'
+import hangman2 from '../public/images/hangman-2.png'
+import hangman3 from '../public/images/hangman-3.png'
+import hangman4 from '../public/images/hangman-4.png'
+import hangman5 from '../public/images/hangman-5.png'
+import hangman6 from '../public/images/hangman-6.png'
+import hangman7 from '../public/images/hangman-7.png'
+
 
 
 export default function HangmanFigure(props) {
     let index = props.imageIndex
-   
+    let images = [
+      hangman0,
+      hangman1,
+      hangman2,
+      hangman3,
+      hangman4,
+      hangman5,
+      hangman6,
+      hangman7
 
+    ]
+    
     
 
   return (
     <div className='hangman-figure'>
-        <Image src={`/images/hangman-${index}.png`} alt='hangman figure' width="400" height='400'/>
-        {/* <Image src={hangman1} alt='hello' height='100' width='100' /> */}
-
+        <Image src={images[index]} alt='hangman figure' width="400" height='400'/>
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+ 
+  images: {
+    domains: ['dc-hangman-app.s3.amazonaws.com'],
+  },
+  
 }
 
 module.exports = nextConfig
