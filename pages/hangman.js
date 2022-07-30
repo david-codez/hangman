e2d442f4086a02062f3017c3b33240ef5c5cbb88
david-codez <davidcodesyt@gmail.com>
2022-07-29 22:36:01 -0400

    vercel deploy

--- pages/hangman.js
+++ pages/hangman.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col, Container } from 'react-bootstrap'
-import Header from '../components/header'
+import Header from '../components/Header'
 import Nav from '../components/Nav'
 import HangmanFigure from '../components/HangmanFigure'
 import LetterButton from '../components/LetterButton'
